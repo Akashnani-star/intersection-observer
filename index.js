d9058e32d590edc85f6e24017648a5bebe76e229
Akashnani-star <63763012+Akashnani-star@users.noreply.github.com>
2021-10-20 10:41:56 +0530

    added website

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-setTimeOut(()=>{
+setTimeout(()=>{
+	let divs = document.querySelectorAll("div")
 
+	const options = {
+		rootMargin: "0px 0px -410px 0px",
+	};
 
-let divs = document.querySelectorAll("div")
+	const observer = new IntersectionObserver((entries,observer)=>{
+		entries.forEach(entry=>{
+			if(entry.isIntersecting){
+				const nodes = entry.target.childNodes;
+				nodes.forEach(node=>{
+					node.src = node.getAttribute("data-src");
+					observer.unobserve(entry.target);
+				})
+			}
+		});
+	},options);
 
-const options = {
-    rootMargin: "0px 0px -400px 0px",
-};
-
-const observer = new IntersectionObserver((entries,observer)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            const nodes = entry.target.childNodes;
-            nodes.forEach(node=>{
-                node.src = node.getAttribute("data-src");
-                observer.unobserve(entry.target);
-            })
-        }
-    });
-},options);
-
-divs.forEach(div=>observer.observe(div));
-
-},3000)
+	divs.forEach(div=>observer.observe(div));
+},2000)
