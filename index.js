728878308db1ee7702be5a033b7c631c11b0f9af
Akashnani-star <dhanviakash1234@gmail.com>
2021-10-20 10:35:14 +0530

    Added website

---
+++ index.js
@@ -0,0 +1,19 @@
+let divs = document.querySelectorAll("div")
+
+const options = {
+    rootMargin: "0px 0px -300px 0px",
+};
+
+const observer = new IntersectionObserver((entries,observer)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            const nodes = entry.target.childNodes;
+            nodes.forEach(node=>{
+                node.src = node.getAttribute("data-src");
+                observer.unobserve(entry.target);
+            })
+        }
+    });
+},options);
+
+divs.forEach(div=>observer.observe(div));
